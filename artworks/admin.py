d98636e5127a221f62a344b7ad6815809ffadfd9
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-09-03 16:11:52 +0300

    add admin classes

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
 from .models import Category, Genre, Title
 
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Title)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    list_filter = ('name',)
+    empty_value_display = _('-empty-',)
+    ordering = ('pk',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    list_filter = ('name',)
+    empty_value_display = _('-empty-',)
+    ordering = ('pk',)
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'description', 'category', 'show_genres')
+    search_fields = ('name', 'year', 'description')
+    list_filter = ('year', 'genre', 'category')
+    empty_value_display = _('-empty-',)
+    ordering = ('name',)
+
+    def show_genres(self, obj):
+        return ", ".join([g.name for g in obj.genre.all()])
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
