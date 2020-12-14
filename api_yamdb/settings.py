77549cc0dbb1d96c5842297cbfaeca13c74f6c58
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-12-14 16:05:24 +0300

    add ngnix.conf and delete STATICFILES_DIRS

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -132,8 +132,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
